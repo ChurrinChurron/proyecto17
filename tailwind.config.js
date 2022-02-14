310815f3f1b4d755c3bde3c7768d1199264567bd
Ana <marianafantasy8@gmail.com>
2022-02-14 16:57:19 -0400

    sunnyside landing page

---
+++ tailwind.config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  content: [
+    './*.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        softred: "hsl(7, 99%, 70%)",
+          yellow: "hsl(51, 100%, 49%)",
+          darkdescyan: "hsl(167, 40%, 24%)",
+          darkblue: "hsl(198, 62%, 26%)",
+          footercyan: "hsl(168, 34%, 41%)",
+          verydarkblue: "hsl(212, 27%, 19%)",
+          verygrayblue: "hsl(213, 9%, 39%)",
+          darkgrayblue: "hsl(232, 10%, 55%)",
+          grayblue: "hsl(210, 4%, 67%)",
+          footer: "hsl(168, 40%, 68%)",
+          footertext: "hsl(168, 37%, 40%)",
+          yellowhover: "hsl(51, 92%, 70%)",
+          opaci: "hsl(198, 98%, 74%)",
+          },
+
+    borderWidth: {
+
+      'right': '30px',
+      'bottom': '30px',
+    },
+
+    inset: {
+
+      "linea": "5.3em",
+    },
+
+    brightness: {
+
+      '5': "5.5",
+    },
+
+    height: {
+
+      'altura': '27rem',
+    },
+
+    letterSpacing: {
+
+      "client": "0.25em",
+      "widest": "0.5em",
+    },
+    },
+
+    fontFamily: {
+			barlow: ["Barlow"],
+			fraunces: ["Fraunces"],
+    },
+  },
+  plugins: [],
+}
